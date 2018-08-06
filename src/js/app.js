19b6af3affdc9056faa67c8db9b2357f7cf7c8cb
shaolin-wu <15265249516@163.com>
2018-08-06 21:43:36 +0800

    bug--->无法完成注册的提交功能

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
-let app
-app = new Vue({
+let app = new Vue({
   el: '#app',
   data: {
     editingName: false,
     loginVisible: false,
-    signUpVisible: false,
+    signUpVisible: true,
     resume: {
       name: '姓名',
       gender: '男',
@@ -12,11 +11,36 @@ app = new Vue({
       jobTitle: '前端工程师',
       email: 'example@example.com',
       phone: '1666666666'
+    },
+    signUp: {
+      userName: '',
+      email: '',
+      password: ''
     }
   },
   methods: {
     onEdit(key, value) {
       this.resume[key] = value
+    },
+    onSignUp(e) {
+      console.log(e);
+      console.log(this)
+      console.log(this.signUp)
+      // 新建 AVUser 对象实例
+      var user = new AV.User();
+      console.log(1);
+      console.log(user);
+      // 设置用户名
+      user.setUsername(this.signUp.userName);
+      // 设置密码
+      user.setPassword(this.signUp.password);
+      // 设置邮箱
+      user.setEmail(this.signUp.email);
+      user.signUp().then(function (user) {
+        console.log(user);
+      }, function (error) {
+        console.log(error);
+      });
     },
     onClickSave() {
       let currentUser = AV.User.current()
